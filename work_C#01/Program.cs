89de46a63b2f5b537c3487176448ffe470900e31
Fray <kozar20000@mail.ru>
2022-06-23 21:28:16 +0300

    Проверка квадрата

--- work_C#01/Program.cs
+++ work_C#01/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Введите число:");
+﻿/*Console.WriteLine("Введите число:");
 int numberA = Convert.ToInt32(Console.ReadLine());
 int sum = numberA * numberA;
-Console.WriteLine(sum);
+Console.WriteLine(sum);*/
+
+Console.WriteLine("Введите число a:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("a является квадратом b");
+}
+else
+{
+    Console.WriteLine("a не является квадратом b");
+}
